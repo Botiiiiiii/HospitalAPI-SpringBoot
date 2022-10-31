167a64bee79cc61a2006eccbdede911dccd8b880
jinwoooooo <song427j@gmail.com>
2022-10-31 15:44:49 +0900

    Factory, 10만건 테스트

--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -1,14 +1,36 @@
 package com.springboot.hello.parser;
 
 import com.springboot.hello.domain.dto.Hospital;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 class HospitalParserTest {
-
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+    @Test
+    @DisplayName("10만건 이상 데이터가 파싱 되는지")
+    void oneHundreadThousandRows() throws IOException {
+        // 서버환경에서 build할 때 문제가 생길 수 있습니다.
+        // 어디에서든지 실행할 수 있게 짜는 것이 목표.
+        String filename = "C:\\Users\\jinwoo\\git\\hello\\fulldata_01_01_02_P_의원.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size() > 1000);
+        assertTrue(hospitalList.size() > 10000);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(hospitalList.get(i).getHospitalName());
+        }
+        System.out.printf("파싱된 데이터 개수:", hospitalList.size());
+    }
     @Test
     void convertToHospital() {
         HospitalParser hp = new HospitalParser();
