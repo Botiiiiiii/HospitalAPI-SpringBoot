ed6f0eecda78b5c5de2787276c9c0f20fb8b6d79
jinwoooooo <song427j@gmail.com>
2022-10-31 15:14:32 +0900

    Hospital paser 1 line Test

--- src/main/java/com/springboot/hello/parser/HospitalParser.java
+++ src/main/java/com/springboot/hello/parser/HospitalParser.java
@@ -12,7 +12,7 @@ public class HospitalParser implements Parser<Hospital> {
         System.out.println(Arrays.toString(row));
 
         Hospital hospital = new Hospital();
-        hospital.setId(Integer.parseInt(row[0].replace("\n","")));
+        hospital.setId(Integer.parseInt(row[0].replace("\"","")));
         hospital.setOpenServiceName(row[1]);
         hospital.setOpenLocalGovernmentCode(Integer.parseInt(row[3]));
         hospital.setManagementNumber(row[4]);
@@ -32,9 +32,9 @@ public class HospitalParser implements Parser<Hospital> {
         hospital.setHealthcareProviderCount(Integer.parseInt(row[29]));
         hospital.setPatientRoomCount(Integer.parseInt(row[30]));
         hospital.setTotalNumberOfBeds(Integer.parseInt(row[31]));
-        hospital.setTotalAreaSize(Integer.parseInt(row[32]));
+        hospital.setTotalAreaSize(Float.parseFloat(row[32].replace("\"","")));
 
-        return new Hospital();
+        return hospital;
     }
 }
 
