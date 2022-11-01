475e86dacb3cfd40df1a718680d39575d0902eee
jinwoooooo <song427j@gmail.com>
2022-11-01 14:34:08 +0900

    DB insert

--- src/main/java/com/springboot/hello/domain/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/domain/dao/HospitalDao.java
@@ -4,15 +4,19 @@ import com.springboot.hello.domain.dto.Hospital;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class HospitalDao {
     private final JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
 
-    public HospitalDao(JdbcTemplate jdbcTemplate) {
+    public HospitalDao(JdbcTemplate jdbcTemplate, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.dataSource = dataSource;
     }
 
-    public void add(Hospital hospital) {
+    public void add(final Hospital hospital) {
         String sql = "INSERT INTO `likelion-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)" +
                 " VALUES (?,?,?," +
                 " ?,?,?," +
--- src/test/java/com/springboot/hello/parser/HospitalParserTest.java
+++ src/test/java/com/springboot/hello/parser/HospitalParserTest.java
@@ -1,5 +1,6 @@
 package com.springboot.hello.parser;
 
+import com.springboot.hello.domain.dao.HospitalDao;
 import com.springboot.hello.domain.dto.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,8 +16,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class HospitalParserTest {
+    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
     @Autowired
     ReadLineContext<Hospital> hospitalReadLineContext;
+    @Autowired
+    HospitalDao hospitalDao;
     @Test
     @DisplayName("10만건 이상 데이터가 파싱 되는지")
     void oneHundreadThousandRows() throws IOException {
@@ -34,9 +38,16 @@ class HospitalParserTest {
         System.out.printf("파싱된 데이터의 수: %d", hospitalList.size());
     }
     @Test
+    @DisplayName("Hospital Insert 테스트")
+    void add(){
+        HospitalParser hp = new HospitalParser();
+        Hospital hospital = hp.parse(line1);
+        hospitalDao.add(hospital);
+    }
+    @Test
     void convertToHospital() {
         HospitalParser hp = new HospitalParser();
-        String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+
         Hospital hospital = hp.parse(line1);
 
         assertEquals(1, hospital.getId()); // col:0
