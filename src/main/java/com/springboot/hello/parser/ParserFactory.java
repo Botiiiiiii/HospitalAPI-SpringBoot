9550a163fb2e363d24173f8a529f2b56b301df90
jinwoooooo <song427j@gmail.com>
2022-11-01 13:57:37 +0900

    ParserFactory

---
+++ src/main/java/com/springboot/hello/parser/ParserFactory.java
@@ -0,0 +1,17 @@
+package com.springboot.hello.parser;
+
+import com.springboot.hello.domain.dto.Hospital;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//IoC와 Di하는 부분(수동)
+
+@Configuration
+public class ParserFactory {
+
+    @Bean
+    public ReadLineContext<Hospital> hospitalReadLineContext() {
+        return new ReadLineContext<Hospital>(new HospitalParser());
+    }
+}
+
